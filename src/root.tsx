800063f6ff13f8c0a46dcad6594aa0b70dc25823
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-07-21 23:47:05 -0300

    first commit

--- src/root.tsx
+++ src/root.tsx
@@ -1,17 +1,45 @@
-import { Counter } from "./components/counter/counter";
-import { Logo } from "./components/logo/logo";
 import "./global.css";
 
 export default () => {
   return (
     <>
       <head>
-        <meta charSet="utf-8" />
-        <title>Qwik Blank App</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='./images/favicon-32x32.png'
+        />
+        <title>Frontend Mentor | QR code component</title>
       </head>
-      <body class="bg-gray-900 px-16">
-        <Logo />
-        <Counter />
+      <body class='py-[85px] px-7 bg-Light-gray'>
+        <div class='bg-white rounded-2xl px-4 pt-4'>
+          <img
+            class='rounded-[9px]'
+            src='/public/images/image-qr-code.png'
+            height={288}
+            width={288}
+            alt='qr code'
+          />
+          <article class='mt-7 text-center leading-4'>
+            <h1 class='font-bold text-2xl text-Dark-blue'>
+              Improve your front-end skills by building projects
+            </h1>
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </article>
+        </div>
+        <div class='attribution'>
+          Challenge by{" "}
+          <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+            Frontend Mentor
+          </a>
+          . Coded by <a href='#'>Your Name Here</a>.
+        </div>
       </body>
     </>
   );
